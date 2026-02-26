99e2d38aef1c55c08c904a9e213e779ba067f576
RSGill24 <rajindergill2024@gmail.com>
2026-02-26 15:04:01 +0530

    Update install_PowerSTIG.ps1

--- packer/scripts/windows/install_PowerSTIG.ps1
+++ packer/scripts/windows/install_PowerSTIG.ps1
@@ -1,1 +1,14 @@
-Install-Module -Name PowerStig -Scope CurrentUser -Force
+# Install NuGet Provider
+Install-PackageProvider -Name NuGet -Force
+ 
+# Trust PSGallery
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+ 
+# Install PowerSTIG
+Install-Module -Name PowerSTIG -Force
+ 
+# Apply Windows Server 2022 STIG
+Import-Module PowerSTIG
+ 
+$stig = Get-Stig -Name WindowsServer -Version 2022
+Invoke-Stig -Stig $stig -Apply

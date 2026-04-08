5f9dcdb0a80bfc4606bdbc7c77a4af7f4d419157
RSGill24 <rajindergill2024@gmail.com>
2026-04-08 14:00:43 +0530

    Update registry_stig.ps1

--- win2022-dev/packer/scripts/registry_stig.ps1
+++ win2022-dev/packer/scripts/registry_stig.ps1
@@ -1,4 +1,4 @@
-﻿Write-Host "=== Applying Registry STIG (Targeted Fix) ==="
+Write-Host "=== Applying Registry STIG (Targeted Fix) ==="
 
 # V-254343.b
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" `
@@ -24,15 +24,5 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 # V-254454 (Idle timeout)
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
   -Name "InactivityTimeoutSecs" -Value 900 -Type DWord -Force
-
-Write-Host "=== Setting DoD Login Banner ==="
-
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
-    -Name "LegalNoticeCaption" -Value "DoD Notice and Consent Banner" -Type String -Force
-
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
-    -Name "LegalNoticeText" -Value "WARNING_____WARNING You are accessing a U.S. Government information system, which includes: 1) this computer, 2) this computer network, 3) all Government-furnished computers connected to this network, and 4) all Government-furnished devices and storage media attached to this network or to a computer on this network. You understand and consent to the following: you may access this information system for authorized use only; unauthorized use of the system is prohibited and subject to criminal and civil penalties. You have no reasonable expectation of privacy regarding any communication or data transiting or stored on this information system. At any time and for any lawful Government purpose, the Government may monitor, intercept, audit, and search and seize any communication or data transiting or stored on this information system, and any communication or data transiting or stored on this information system may be disclosed or used for any lawful Government purpose. This information system may contain Controlled Unclassified Information (CUI) that is subject to safeguarding or dissemination controls in accordance with law, regulation, or Government-wide policy. Accessing and using this system indicates your understanding of this warning." -Type String -Force
-
-Write-Host "=== DoD Login Banner Applied ==="
 
 Write-Host "=== Registry STIG Fixed ==="
